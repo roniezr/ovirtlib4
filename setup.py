f53fc017a08a0003c88841d4a363c2aab7ee820f
reliezer <reliezer@redhat.com>
2020-07-22 20:55:12 +0300

    Fix bug: utils module was not installed

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from distutils.core import setup
 setup(
   name='ovirtlib4',
-  packages=['ovirtlib4'],  # this must be the same as the name above
+  packages=['ovirtlib4', 'ovirtlib4.utils'],
   license='Apache License 2.0',
-  version='1.0.0',
+  version='1.0.1',
   description='Ovirtsdk4 wrapper',
   long_description=open('README.rst').read(),
   author='Roni Eliezer',
   author_email='reliezer@redhat.com',
-  url='https://github.com/roniezr/ovirtlib4',  # use the URL to the github repo
-  download_url='https://github.com/roniezr/ovirtlib4-1.0.0.tar.gz',
+  url='https://github.com/rhevm-qe-automation/ovirtlib4',
+  download_url='https://github.com/rhevm-qe-automation/ovirtlib4-1.0.1.tar.gz',
   keywords=['red hat', 'virtualization', 'ovirt', 'ovirtsdk'],
   classifiers=[],
   install_requires=['pycurl', 'ovirt-engine-sdk-python'],
